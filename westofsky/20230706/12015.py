15daa616bbe7a575bfdb6d7bb9ef4ed1c49df779
westofsky <westofsky159@gmail.com>
2023-07-06 21:47:33 +0900

    [BOJ] 12015 가장 긴 증가하는 부분 수열

---
+++ westofsky/20230706/12015.py
@@ -0,0 +1,26 @@
+import sys
+
+def find(target):
+  start, end = 1, len(dist) - 1
+  while start < end :
+    mid = (start + end) // 2
+    if dist[mid] < target :
+      start = mid + 1
+    elif dist[mid] > target :
+      end = mid
+    else :
+      start = end = mid
+  return end
+
+
+n = int(input())
+array = list(map(int, sys.stdin.readline().split()))
+dist = [0]
+
+for i in array :
+  if dist[-1] < i :
+    dist.append(i)
+  else :
+    dist[find(i)] = i
+
+print(len(dist) - 1)
